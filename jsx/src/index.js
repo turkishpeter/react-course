e661714f35e160464e4a2143578f2c6dcb842cd8
Péter Török <turkishpeter2@gmail.com>
2019-06-19 12:54:20 +0200

    init-both-folders

---
+++ jsx/src/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function getButtonText() {
+  return "Click On MEsdadad !asdasd";
+}
+
+const App = () => {
+  const buttonText = { text: "Click me" };
+  const labelText = "Enter name:";
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.text}
+      </button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+
+setInterval(() => {
+	
+}, interval);
